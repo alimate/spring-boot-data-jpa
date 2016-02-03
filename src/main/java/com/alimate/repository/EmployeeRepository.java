cc3cbb96809ae1949a21c94ff11bde798f76abc0
Ali Dehghani <ali.dehghani.g@gmail.com>
2016-02-03 22:43:09 +0330

    Added Spring MVC

--- src/main/java/com/alimate/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alimate.repository;
-
-import com.alimate.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {}
--- src/main/java/com/alimate/service/GreetingService.java
+++ src/main/java/com/alimate/service/GreetingService.java
@@ -1,14 +1,15 @@
 package com.alimate.service;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 @RequestMapping("/greet")
 public class GreetingService {
-    @RequestMapping(method = RequestMethod.GET)
-    public String doGreet() {
-        return "Hello";
+    @RequestMapping(method = GET)
+    public String itWorks() {
+        return "It Works";
     }
 }
